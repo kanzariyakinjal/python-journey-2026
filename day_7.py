f3963f2c091403b23e9732fb2831ba462ffeaf0d
kanzariyakinjal <kanzariyakinjal2007@gmail.com>
2026-04-02 00:29:31 +0530

    Day 7: string methods
    learned about python string methods 
    applied
    len( ) to count characters in a string
    upper( ) and lower( ) methods
    find( ) to locate substrings
    replace( ) to modify strings
    'in' operator for checking substrings

---
+++ day_7.py
@@ -0,0 +1,18 @@
+#string methods
+course = ' python for beginners '
+print(len(course))
+# string method
+course = ' python for beginners '
+print(course.upper())
+print(course.lower())
+# find method
+course = 'python for beginners'
+print(course.find('p'))
+print(course. find('o'))
+print(course.find('beginners'))
+# replace method
+print(course.replace('beginners','absolute beginners'))
+print(course.replace('p','j'))
+# in oprator
+course = 'python for beginners'
+print('python' in course)
