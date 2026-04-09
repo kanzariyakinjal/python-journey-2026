0d583762cf3e0adf9cc7e8eb86005bc0470abe4c
kanzariyakinjal <kanzariyakinjal2007@gmail.com>
2026-04-09 17:49:21 +0530

    day 4: strings
    learned python strings( indexing, slicing, multi-line strings) with practice examples.

--- day_4(1).py
+++ day_4(1).py
@@ -1,2 +1,25 @@
 course = "python's course for beginners" 
 print(course)
+# accessing characters
+print(course[0])  #p
+print(course[-1]) #s
+
+#slicing
+print(course[0:3]) #pyt
+print(course[1:])  #ython for beginners
+print(course[:5])  #pytho
+
+#copy string
+another = course[:]
+print(another)
+
+#multi-line string
+message = """
+hi john,
+here is our first email to you.
+thank you,
+the support team
+"""
+print(message)
+
+      
