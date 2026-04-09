2f0ac43895f137c1d10e789828b6e45ee18c3524
kanzariyakinjal <kanzariyakinjal2007@gmail.com>
2026-04-09 17:35:35 +0530

    Day 8 : arithmetic  operators

---
+++ day_8.py
@@ -0,0 +1,13 @@
+#arith matic oprators
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+# augmented assignment operator
+x = 10
+x = x + 3
+x += 3
+print(x)
